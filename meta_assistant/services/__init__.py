c74387822852a09471b710e28752b6c92f5d3e1a
Di Fang <fti@mail.scut.edu.cn>
2023-11-12 22:59:42 +0800

    fix: 在__init__.py文件自动导入TextSummary类

--- meta_assistant/services/__init__.py
+++ meta_assistant/services/__init__.py
@@ -3,3 +3,4 @@ from .text_generation import TextGenerator
 from .text_to_speech import TextToSpeech
 from .audio2face import Audio2Face
 from .audio2chunks import Audio2Chunks
+from .text_summary import TextSummary
