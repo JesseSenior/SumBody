d3dba88d281e4ce2e561ee15cf8991f8c1670655
1722087564 <127661029+1722087564@users.noreply.github.com>
2023-11-30 09:48:20 +0800

    Update __init__.py

--- sumbody/services/__init__.py
+++ sumbody/services/__init__.py
@@ -3,3 +3,4 @@ from .audio2face import Audio2Face
 from .audio2chunks import Audio2Chunks
 from .text_summary import TextSummary
 from .stt_client import STTClient
+from .tts_client import TTSClient
